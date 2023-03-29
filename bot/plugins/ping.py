2103345eaa7d8395ee49ca63bd3cf7fbba13867c
AbrahamAogiri <128735289+AbrahamAogiri@users.noreply.github.com>
2023-03-29 15:24:26 +0530

    Update ping.py

--- bot/plugins/ping.py
+++ bot/plugins/ping.py
@@ -13,10 +13,10 @@ async def ping_handler(c: Client, m: "types.Message"):
     await add_user_to_database(c, m)
     await c.send_flooded_message(
         chat_id=m.chat.id,
-        text="Hi, I am Rename Bot!\n\n"
+        text="Hi {mention}, I am ᴀʟᴘʜᴇ ʀᴇɴᴀᴍᴇ Bot!\n\n"
              "I can rename media without downloading it!\n"
-             "Speed depends on your media DC.\n\n"
-             "Just send me media and reply to it with /rename command.",
+             "Just send me media and reply to it with /rename command."
+             "Bot created by: @Lucifer_x0o",
         reply_markup=types.InlineKeyboardMarkup([[
            types.InlineKeyboardButton("Show Settings",
                                       callback_data="showSettings")
@@ -35,7 +35,8 @@ async def help_handler(c: Client, m: "types.Message"):
              "Speed depends on your media DC.\n\n"
              "Just send me media and reply to it with /rename command.\n\n"
              "To set custom thumbnail reply to any image with /set_thumbnail\n\n"
-             "To see custom thumbnail press /show_thumbnail",
+             "To see custom thumbnail press /show_thumbnail"
+             "Cantact @Lucifer_x0o for more help",
         reply_markup=types.InlineKeyboardMarkup([[
            types.InlineKeyboardButton("Show Settings",
                                       callback_data="showSettings")]])
